0c817cbacca95bcbe71d2fae198a6caa81d6817d
Olya-Kravchuk <143799228+Olya-Kravchuk@users.noreply.github.com>
2024-08-29 16:20:50 +0200

    в

---
+++ app/+html.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const HtmlComponent = () => {
+  return (
+    <html>
+      <head>
+        <title>Мій React Native проект</title>
+      </head>
+      <body>
+        <div id="root"></div>
+      </body>
+    </html>
+  );
+};
+
+export default HtmlComponent;
